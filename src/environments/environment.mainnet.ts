cc40783edfbf30bd4eec387dccdf3166649b5c0c
Tihomir Stoychev Stoychev <tihomir.stoychev@mhp-net.de>
2023-01-07 19:23:59 +0100

    Changed active tract

--- src/environments/environment.mainnet.ts
+++ src/environments/environment.mainnet.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   mainnet:true,
   MATIC_FEE: 0.0055,
-  MATIC_DEPLOYED_ADDRESS_OTC: '0x137c7aD9eb23D7F2A9176fc83A57dD0Fa8841f6e',
+  MATIC_DEPLOYED_ADDRESS_OTC: '0x20cc53305B639EB2FB9aa1fed26D9F0b95a3a183',
 };
