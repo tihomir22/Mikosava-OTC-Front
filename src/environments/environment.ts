c5dd00ee2dc32151d75a63a5dd352d39ff5bfaf3
Tihomir Stoychev Stoychev <tihomir.stoychev@mhp-net.de>
2023-02-26 17:04:41 +0100

    Changed deployed address

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   mainnet: false,
   MATIC_FEE: 0.0055,
-  MATIC_DEPLOYED_ADDRESS_OTC: '0x6142df089d5873415821ee8ad248BB9e7b1d4203',
+  MATIC_DEPLOYED_ADDRESS_OTC: '0x81b47E8520f9E7431D6135fa6Bb60F2a4E221Fdc',
   ALCHEMY_API: '_Nl2yOagivWBbjDwBg29FYZub2iOcVTz',
 };
 
